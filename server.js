c1292bd1978c6510d53f0415cd1d4485d564d6fe
ryo <ryoutakaya3623@Yahoo.co.jp>
2020-08-23 13:20:24 +0900

    fixed

--- server.js
+++ server.js
@@ -2,8 +2,12 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-const port = process.env.PORT || 3002;
-app.use(express.static(path.resolve(__dirname + "/build")));
+const publicPath = path.join(__dirname, "..", "public");
+const port = process.env.PORT || 3000;
+app.use(express.static(publicPath));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(publicPath, "index.html"));
+});
 
 app.listen(port, () => {
   console.log("port", port);
